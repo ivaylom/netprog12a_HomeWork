6fbb6212e210c34d3db9a60c837ec09d00667baf
Marina Tihova <marina@debian>
2017-10-24 20:52:17 +0300

    Add Servlet homework

---
+++ Servlet/20/ChatHW/src/org/elsys/netprog/chat/Message.java
@@ -0,0 +1,21 @@
+package org.elsys.netprog.chat;
+
+public class Message {
+	private String name;
+	
+	private String message;
+	
+	
+	public Message(String name, String message) {
+		this.name = name;
+		this.message = message;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+}
